81f94d2a9c248804addeee5fe51a6b8850a7e509
Pavnuto <chuvachek@list.ru>
2022-11-25 17:13:32 +0300

    Add firebase tools

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB41nIkb7HSTd_i4_yhdyXDBQWPFLTIrtc",
+  authDomain: "reactchat-ffcc4.firebaseapp.com",
+  projectId: "reactchat-ffcc4",
+  storageBucket: "reactchat-ffcc4.appspot.com",
+  messagingSenderId: "120607505812",
+  appId: "1:120607505812:web:e29a472678cded22f82381",
+  measurementId: "G-Y5TPB41DGE"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
